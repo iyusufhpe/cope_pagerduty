b863e8b5e8c5a27d4e229df104a8df5ce4e1bfe2
Iqbal ASUS Win11 <iqbalyusufdipu@gmail.com>
2024-09-29 15:52:40 -0500

    duckdb analysis

--- fetch-alerts.py
+++ fetch-alerts.py
@@ -1,20 +1,31 @@
 import time
 import json
 from pdpyras import APISession
+from token_getter import api_token
+import csv
 
 # Initialize the PagerDuty API session
-api_token = 'your_api_token'
 session = APISession(api_token)
+
+
+def save_to_file(incident_id, data, filename):
+    try:
+        with open(filename, 'r') as file:
+            alerts_list = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        alerts_list = []
+
+    alerts_list.append({incident_id: data})
+
+    with open(filename, 'w') as file:
+        json.dump(alerts_list, file, indent=4)
+
 
 def fetch_alerts_for_incident(incident_id):
     # Fetch alerts for a specific incident
     response = session.rget(f'/incidents/{incident_id}/alerts')
     return response
 
-def save_to_file(incident_id, data, filename='incident_alerts.json'):
-    # Save each response to a file in JSON format
-    with open(filename, 'a') as file:
-        file.write(json.dumps({incident_id: data}, indent=4) + '\n')
 
 def batch_download(incident_ids, filename='incident_alerts.json'):
     batch_size = 960
@@ -34,6 +45,21 @@ def batch_download(incident_ids, filename='incident_alerts.json'):
             print("Pausing for 1 minute to respect rate limit...")
             time.sleep(60)
 
+
+def read_incident_ids_from_csv(csv_file_path):
+    incident_ids = []
+    with open(csv_file_path, mode='r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            incident_ids.append(row[0])  # Assuming incident IDs are in the first column
+    return incident_ids
+
+
+# # Example usage
+# incident_ids = ['Q23RA58KK63575', 'Q0FUHDIJXCD1Q4', 'Q0HBWO2BH6UMAQ', 'Q3ZGOBT8W3GWW5', 'Q3XDFUVJR2D45U']  # Replace with your actual list of incident IDs
+# batch_download(incident_ids)
+
 # Example usage
-incident_ids = ['id1', 'id2', 'id3', ...]  # Replace with your actual list of incident IDs
+csv_file_path = 'D:\\Data\\hpe\\pagerduty\\incidents_ids_aug2024.csv'
+incident_ids = read_incident_ids_from_csv(csv_file_path)
 batch_download(incident_ids)
